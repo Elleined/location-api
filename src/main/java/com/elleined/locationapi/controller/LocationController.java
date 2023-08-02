c9daf7c9caf7ab4ab30aaff0dc31f40a4cb7938e
Elleined <demadegu@gmail.com>
2023-08-02 19:36:50 +0800

    addrress services are now have their own controller

---
+++ src/main/java/com/elleined/locationapi/controller/LocationController.java
@@ -0,0 +1,4 @@
+package com.elleined.locationapi.controller;
+
+public class LocationController {
+}
--- src/main/java/com/elleined/locationapi/controller/UserController.java
+++ src/main/java/com/elleined/locationapi/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.elleined.locationapi.controller;
 
-import com.elleined.locationapi.dto.AddressDTO;
 import com.elleined.locationapi.dto.UserDTO;
 import com.elleined.locationapi.service.LocationService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,28 +25,5 @@ public class UserController {
     @GetMapping("/{currentUserUUID}")
     public UserDTO getByUUID(@PathVariable("currentUserUUID") String currentUserUUID) {
         return locationService.getByUUID(currentUserUUID);
-    }
-
-    @PostMapping("/userAddress/{currentUserUUID}")
-    public AddressDTO saveUserAddress(@PathVariable("currentUserUUID") String currentUserUUID,
-                                      @Valid @RequestBody AddressDTO addressDTO) {
-
-        return locationService.saveUserAddress(currentUserUUID, addressDTO);
-    }
-
-    @PostMapping("/deliveryAddresses/{currentUserUUID}")
-    public AddressDTO saveDeliveryAddress(@PathVariable("currentUserUUID") String currentUserUUID,
-                                          @Valid @RequestBody AddressDTO addressDTO) {
-        return locationService.saveDeliveryAddress(currentUserUUID, addressDTO);
-    }
-
-    @GetMapping("/userAddress/{currentUserUUID}")
-    public AddressDTO getAddress(@PathVariable("currentUserUUID") String currentUserUUID) {
-        return locationService.getAddress(currentUserUUID);
-    }
-
-    @GetMapping("/deliveryAddresses/{currentUserUUID}")
-    public Set<AddressDTO> getDeliveryAddresses(@PathVariable("currentUserUUID") String currentUserUUID) {
-        return locationService.getDeliveryAddresses(currentUserUUID);
     }
 }
