f700cdcc1998802353f5e3660f32dab4f077f850
Elleined <demadegu@gmail.com>
2023-08-31 23:22:56 +0800

    removed discovery client will be added again soon

--- src/main/java/com/elleined/locationapi/LocationApiApplication.java
+++ src/main/java/com/elleined/locationapi/LocationApiApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableTransactionManagement
 public class LocationApiApplication {
 
